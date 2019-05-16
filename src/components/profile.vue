005cc6b97b8a5b4ecf83fa6e839b4956973a324e
DebraJohn <debra.zhuang@zeasn.com>
2019-05-16 12:03:22 +0800

    更改配置

---
+++ src/components/profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="profile"></div>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.profile {
+  width: 20rem;
+  height: 30rem;
+  border: 1px solid black;
+}
+</style>
+
