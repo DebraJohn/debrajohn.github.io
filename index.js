297da307e2124067ed82509a6ecf5fc416e0c22e
DebraJohn <debra.zhuang@zeasn.com>
2020-09-25 19:31:22 +0800

    修改端口号

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname)))
-app.listen(8080, () => {
-  console.log('App listening at port 8080')
+app.listen(80, () => {
+  console.log('App listening at port 80')
 })
